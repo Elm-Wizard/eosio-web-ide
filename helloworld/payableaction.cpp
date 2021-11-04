7916e52ec884ad38e1cbec8f4448f34d5ab26c30
Elm-Wizard <nikoladuke97@gmail.com>
2021-11-04 08:26:03 +0000

    eosio.token implement and payableaction

--- helloworld/payableaction.cpp
+++ helloworld/payableaction.cpp
@@ -1,16 +1,5 @@
 #include <eosio/eosio.hpp>
 
-CONTRACT payableaction : public contract {
-    public:
-        using contract::contract;
+using namespace eosio;
 
-
-        [[eosio::on_notify("eosio.token::transfer")]]
-        void depositnlock(name from, name to, eosio::asset quantity, std::string memo)
-        {
-            check ((to == get_self() && from != get_self()), "Ne placaj sebi!" );
-            check (quantity.amount > 0, "Ne moze negativno");
-            check (quantity.symbol == hodl_symbol, "Pogresan token!");
-        }
-        // KREIRATI akciju sutra deposit
-}
+CONTRACT hodl
